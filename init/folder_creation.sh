08090d34eebdd3bb3899b5da359d60568c4b4cd7
Sven Kicherer <sven.kicherer@yahoo.com>
2019-07-12 03:41:18 -0700

    integration of mqtt and deconz into pimatic

--- init/folder_creation.sh
+++ init/folder_creation.sh
@@ -3,12 +3,14 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BASEFOLDER="$(dirname "$SCRIPTPATH")"
 
-echo $SCRIPTPATH
-echo $BASEFOLDER
+#echo $SCRIPTPATH
+#echo $BASEFOLDER
 
 compose_input="$SCRIPTPATH/docker-compose.template"
 compose_output_filename="docker-compose.yml"
 compose_output="$BASEFOLDER/compose/$compose_output_filename"
+
+set -x
 
 mkdir "$BASEFOLDER/compose"
 
@@ -28,6 +30,11 @@ chmod -R 777 "$BASEFOLDER/mqtt/log"
 cp "$SCRIPTPATH/mosquitto.conf" "$BASEFOLDER/mqtt/config/mosquitto.conf"
 
 chmod -R 777 "$BASEFOLDER/mqtt/config/mosquitto.conf"
+
+docker build -f "$SCRIPTPATH/pimatic_0.9.50_dockerfile" -t pimatic_0.9.50 .
+
+#docker run --name pimatic -dt -p 80:80 pimatic_0.9.50
+#docker build -f pimatic_0.9.50_dockerfile -t pimatic_0.9.50 .
 
 
 #mkdir deconz
